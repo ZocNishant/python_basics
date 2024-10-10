fdee97a6c9d53eaa29c8a93d5fbb77c1579f9a96
Nishant Zoc <zocnishant@gmail.com>
2024-10-10 22:59:51 +0545

    Ch 1 Done

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-n = "Nishant Joshi"
-print(n)
+# name = "Nishant Joshi"
+# name = "Pasa"
+
+# age = 23
+
+# price = 25.55
+
+# none = None
+
+# print(f"My name is {name} and age is {age}.")
+# print(type(none), none)
+
+# Sum of 2 numbers
+
+# def sumOfDigit():
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
+
+#     sum = a + b
+#     print(sum)
+
+# sumOfDigit()
+
