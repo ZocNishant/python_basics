765b55901d96377942a294a704d118fa06438ab6
Nishant Zoc <zocnishant@gmail.com>
2024-10-15 17:32:30 +0545

    Madlib

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+# Generally like fill in the blanks but form the users choice.
+name = input("Name: ")
+address = input("Address: ")
+gameYouWantToPlay = input("Game You Want To Play: ")
+
+
+madlib = f"Hello {name}. How are you doing {name}. So, today we are here at {address} and we are with {name} and we are playing {gameYouWantToPlay}. Certainly we have not herd of this game {gameYouWantToPlay}. How do we play this game??"
+
+print(madlib)
