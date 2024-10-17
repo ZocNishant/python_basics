adf0a47366a2e0f2c1ab89b0f395f93d13401936
Nishant Zoc <zocnishant@gmail.com>
2024-10-17 08:25:22 +0545

    rock paper scissors

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -1,28 +1,46 @@
 import random
 
-def play():
-    computer_choice = random.choice(['r', 'p', 's'])
-    print(computer_choice)
+# def play():
+#     computer_choice = random.choice(['r', 'p', 's'])
+#     print(computer_choice)
 
-    user_input = input("'r' for rock, 's' for scissors and 'p' for paper")
-
-
-    if user_input == 'r' and computer_choice == 's':
-        print("User has won the game.")
-    elif user_input == 's' and computer_choice == 'p':
-        print("User has won the game.")
-    elif user_input == 'p' and computer_choice == 'r':
-        print("User has won the game.")
-    elif user_input == computer_choice:
-        print("The game has tied.")
-    else:
-        print("Computer wins the game.")
+#     user_input = input("'r' for rock, 's' for scissors and 'p' for paper")
 
 
+#     if user_input == 'r' and computer_choice == 's':
+#         print("User has won the game.")
+#     elif user_input == 's' and computer_choice == 'p':
+#         print("User has won the game.")
+#     elif user_input == 'p' and computer_choice == 'r':
+#         print("User has won the game.")
+#     elif user_input == computer_choice:
+#         print("The game has tied.")
+#     else:
+#         print("Computer wins the game.")
 
-play()
 
 
+# play()
+
+def khelam():
+    user = input("'r' for rock, 's' for scissors and 'p' for paper")
+    computer = random.choice(['r','p','s'])
+
+    if user == computer:
+        return "It's a tie."
+    
+    if is_winner(user, computer):
+        return 'You Won.'
+
+    return "You Lost."
+    
+
+def is_winner(player, opponent):
+    if(player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+
+print(khelam())
 
 # Rock beats Scissors.
 # Scissors beat Paper.
